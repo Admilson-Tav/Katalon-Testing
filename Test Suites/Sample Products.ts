6790526312386ccffaf822d7b2f5daebf4584d74
Admilson Tavares <Admilson Tavares@DESKTOP-G85HJJ4.home>
2023-07-04 12:40:50 +0100

    V2 - Sample Products

---
+++ Test Suites/Sample Products.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sample Products</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9aafcf38-09eb-492c-8c23-5b101aad98c6</testSuiteGuid>
+   <testCaseLink>
+      <guid>8e48a0e2-7444-4f0e-b348-b190cfff89a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample Products/Sample Products - Edit Product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
